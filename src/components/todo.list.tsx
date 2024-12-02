84a1824fdc8d04c5ab6d8d77cb1df7d07cf92629
longtq32 <longtq32@gmail.com>
2024-12-02 22:28:46 +0700

    rename jsx to tsx

---
+++ src/components/todo.list.tsx
@@ -0,0 +1,9 @@
+
+
+const TodoList = () => {
+    return (
+        <div>todo list tsx update</div>
+    )
+}
+
+export default TodoList;
---
+++ src/layout.tsx
@@ -0,0 +1,15 @@
+import TodoList from "./components/todo.list"
+
+function Layout() {
+
+  return (
+
+    <>
+    <div>hello world</div>
+    <TodoList/>
+    </>
+    
+  )
+}
+
+export default Layout
